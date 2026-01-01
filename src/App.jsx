a01a43a91a9dbb484536dd043f11953dbe8cf4ea
Ajit Shukla <official.aj.shukla@gmail.com>
2026-01-02 02:18:35 +0530

    Initial commit of the project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ResumeForm from './components/ResumeForm'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-6">
+      <h1 className="text-3xl font-bold mb-4 text-center">🧠 AI Resume Customizer</h1>
+      <ResumeForm />
+    </div>
+  )
+}
+
+export default App
